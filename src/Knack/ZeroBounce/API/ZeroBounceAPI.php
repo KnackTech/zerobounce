44b3305e7c8dce99b8331528a8b78b192edeb5e7
Doug Woodrow <doug@dougwoodrow.com>
2019-09-18 17:42:12 -0400

    Deal with situation where dotenv isn't loaded

--- src/Knack/ZeroBounce/API/ZeroBounceAPI.php
+++ src/Knack/ZeroBounce/API/ZeroBounceAPI.php
@@ -1,6 +1,8 @@
 <?php
 
+use Dotenv\Dotenv;
 use GuzzleHttp\Client;
+use Knack\ZeroBounce\API\ZeroBounce;
 use Knack\ZeroBounce\Exceptions\ResponseException;
 
 class ZeroBounceAPI {
@@ -13,6 +15,11 @@ class ZeroBounceAPI {
     public function __construct( $key ) {
         $this->key    = $key;
         $this->client = new Client();
+
+        if(!function_exists('getenv')) {
+            $dotenv = new Dotenv( __DIR__ . '/../../../../' );
+            $dotenv->load();
+        }
     }
 
     /**
